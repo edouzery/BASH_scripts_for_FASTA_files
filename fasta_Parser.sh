1aa47b9c70da9126ec75a5021f9d49c0b70829f4
Emmanuel Douzery <95428007+edouzery@users.noreply.github.com>
2023-04-25 12:31:55 +0000

    Update fasta_Parser.sh

--- fasta_Parser.sh
+++ fasta_Parser.sh
@@ -32,7 +32,7 @@ fi ;
 
 ## VARIABLES. ##
 
-nbSeq=$(sed '/^$/d' $1 | grep -c '>') ;                                 # Number of sequences
+nbSeq=$(sed '/^$/d' $1 | grep -c '>') ;                         # Number of sequences
 nbSeqLines=$(sed '/^$/d' $1 | grep -c -v '>') ;                 # Number of lines of sequences
 
 
@@ -46,17 +46,17 @@ fi ;
 
 ## CHECK FASTA SEQUENCE LENGTH = ARE SEQUENCES ALIGNED? ##
 
-sed '/^$/d' $1 | grep -v '>' > ${1}.seq.tmp ;                   # Writing sequence lines
+sed '/^$/d' $1 | grep -v '>' > ${1}.seq.tmp ;           # Writing sequence lines
 sed '/^$/d' $1 | grep '>' | tr -d '>' > ${1}.ID.tmp ;   # Writing IDentifiers
 
-for (( l = 1 ; l < $nbSeqLines ; l++ )) ;                                       # l => Numbering of each line
-        do      m=$(expr $l + 1) ;                                                              # m => Numbering of next line
+for (( l = 1 ; l < $nbSeqLines ; l++ )) ;               # l => Numbering of each line
+        do      m=$(expr $l + 1) ;                      # m => Numbering of next line
                 if [ "$(sed -n "${l}p" ${1}.seq.tmp | wc -m)" != "$(sed -n "${m}p" ${1}.seq.tmp | wc -m)" ] ;
                         # then  echo -e '\E[31;3m' "\033[1m Problem with $(sed -n "${l}p" ${1}.ID.tmp) and $(sed -n "${m}p" ${1}.ID.tmp) ... Aligned sequences do not have the same LENGTH ! \033[0m" ;
                         # then    printf "$boldRed \n Aargh :-( $1 has a problem with $(sed -n "${l}p" ${1}.ID.tmp) and $(sed -n "${m}p" ${1}.ID.tmp) ... Aligned sequences do not have the same LENGTH ! \n\n$noColor" ;
                         then    printf "$boldRed \n \t Aargh :-( $1 has a problem with $(sed -n "${m}p" ${1}.ID.tmp) ... Previous sequences in the alignment do not have the same LENGTH ! \n\n$noColor" ;
                                         if [ -f ${1}.seq.tmp ] ; then rm ${1}.seq.tmp ; fi ;    # Post-cleaning
-                                        if [ -f ${1}.ID.tmp ] ; then rm ${1}.ID.tmp ; fi ;              # Post-cleaning
+                                        if [ -f ${1}.ID.tmp ] ; then rm ${1}.ID.tmp ; fi ;      # Post-cleaning
                                         exit ;
                 fi ;
 done ;
