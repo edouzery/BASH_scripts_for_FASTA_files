f567baa4fa5732e1103e1027832e3fd7c0f52e8e
Emmanuel Douzery <95428007+edouzery@users.noreply.github.com>
2025-12-10 12:59:04 +0100

    Enhance color handling and update author info
    Updated author information and improved color handling in terminal output.

--- fasta_Keep_Longest/fasta_Keep_Longest.sh
+++ fasta_Keep_Longest/fasta_Keep_Longest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## AUTHOR : Emmanuel Douzery, May 2013 | March 2015 | March, May 2023. ##
+## AUTHOR : Emmanuel Douzery, May 2013 | Mar. 2015 | Mar., May 2023 | Dec. 2025. ##
 
 ## What does this script do? It outputs the longest sequence from a FASTA file ! ##
 
@@ -8,12 +8,28 @@
 
 ## WARNINGS. ##
 
-boldBlack='\033[0;30m' ;
-boldDarkGray='\033[1;30m' ;
-boldBlue='\033[1;94m' ;
-boldGreen='\033[1;32m' ;
-boldRed='\033[1;91m' ;
-noColor='\033[0m' ;
+# Evaluate if the output is a terminal or a file in order to (de)activate colors.
+if [ -t 1 ]; 
+	then	USE_COLOR=true ;	# For terminal only.
+	else    USE_COLOR=false ;	# Avoiding uninterpreted color codes in the output file.
+fi ;
+
+# Defining the terminal color codes.
+if [ "$USE_COLOR" = true ]; 
+	then	boldBlack='\033[0;30m';
+			boldDarkGray='\033[1;30m' ;
+    		boldBlue='\033[1;94m';
+    		boldGreen='\033[1;32m';
+    		boldRed='\033[1;91m';
+    		noColor='\033[0m';
+	else
+    		boldBlack='';
+			boldDarkGray='' ;
+		    boldBlue='';
+		    boldGreen='';
+		    boldRed='';
+		    noColor='';
+fi ;
 
 if [ -z $1 ] ;
 	then printf "$boldRed \n Please provide a FASTA file (uniline) ! \n\n $noColor" ; exit ;
@@ -25,8 +41,8 @@ fi ;
 
 ## VARIABLES. ##
 
-prefix=${1%.*} ;					# Prefix of the FASTA file name.
-numSeq=$(sed '/^$/d' $1 | grep -c '>') ;		# Number of sequences.
+prefix=${1%.*} ;									# Prefix of the FASTA file name.
+numSeq=$(sed '/^$/d' $1 | grep -c '>') ;			# Number of sequences.
 numSeqLines=$(sed '/^$/d' $1 | grep -c -v '>') ;	# Number of lines of sequences.
 
 
